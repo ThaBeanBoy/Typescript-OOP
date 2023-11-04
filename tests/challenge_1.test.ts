f57c022a796158985b92d3e5615aff8c02d9e53b
ThaBeanBoy <gtchipoyera@gmail.com>
2023-11-04 19:36:27 +0200

    Made some UserClass tests in the tests/change_1.test.ts file

--- tests/challenge_1.test.ts
+++ tests/challenge_1.test.ts
@@ -7,8 +7,42 @@ const testValues: UserClassConstructorProps = {
   email: 'n_uzumaki@konoha.vill',
 };
 
-test('UserClass constructor working', () => {
-  const user = new UserClass(testValues);
+const testUser = new UserClass(testValues);
 
-  expect(user.firstName).toBe(testValues.firstName);
+test('UserClass constructor & getters working', () => {
+  expect(testUser.firstName).toBe(testValues.firstName);
+  expect(testUser.lastName).toBe(testValues.lastName);
+  expect(testUser.email).toBe(testValues.email);
+});
+
+test('Getting Full name from user', () => {
+  expect(testUser.fullName).toBe(`${testUser.firstName} ${testUser.lastName}`);
+});
+
+test('Email checking', () => {
+  // checking invalid email
+  expect(testUser.checkEmail('wrong_email@wrong.com')).toBe(false);
+
+  // checking valid email
+  expect(testUser.checkEmail(testValues.email)).toBe(true);
+});
+
+test('Setters working', () => {
+  const newTestValues: UserClassConstructorProps = {
+    firstName: 'sasuke',
+    lastName: 'uchiha',
+    email: 'sasuke@akatsuki.org',
+  };
+
+  // testing first name value change
+  testUser.firstName = newTestValues.firstName;
+  expect(testUser.firstName).toBe(newTestValues.firstName);
+
+  // testing last name value change
+  testUser.lastName = newTestValues.lastName;
+  expect(testUser.lastName).toBe(newTestValues.lastName);
+
+  // testing email value change
+  testUser.email = newTestValues.email;
+  expect(testUser.email).toBe(newTestValues.email);
 });
