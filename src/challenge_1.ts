efd992f660474f6523ea56127dff798524950c38
ThaBeanBoy <gtchipoyera@gmail.com>
2023-11-04 18:35:43 +0200

    Finished making the UserClass for challenge 1

---
+++ src/challenge_1.ts
@@ -0,0 +1,55 @@
+export class UserClass {
+  // Properties
+  private _firstName: string;
+  private _lastName: string;
+  private _email: string;
+
+  // Constructor
+  constructor({
+    firstName,
+    lastName,
+    email,
+  }: {
+    firstName: string;
+    lastName: string;
+    email: string;
+  }) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._email = email;
+  }
+
+  // Getters & Setters
+  get fullName(): string {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get firstName(): string {
+    return this._firstName;
+  }
+
+  set firstName(value: string) {
+    this._firstName = value;
+  }
+
+  get lastName(): string {
+    return this._lastName;
+  }
+
+  set lastName(value: string) {
+    this._lastName = value;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  set email(value: string) {
+    this._email = value;
+  }
+
+  // Utility method
+  checkEmail(emailToBeChecked: string): boolean {
+    return this._email === emailToBeChecked;
+  }
+}
